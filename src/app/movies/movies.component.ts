34feeb0ecdfa9f0f3f265ff800c6184d60ea5036
luyandamncube <59448873@mylife.unisa.ac.za>
2019-01-27 23:07:41 +0200

    changed styling of movie posters

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -4,8 +4,10 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 import { DataSource } from '@angular/cdk/collections';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
-import { DataService } from '../data.service';
-
+import { DataService } from '../services/data.service';
+//Mobile view breekpoints
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movies',
@@ -13,15 +15,29 @@ import { DataService } from '../data.service';
   styleUrls: ['./movies.component.scss']
 })
 export class MoviesComponent implements OnInit {
+  loading = false;
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
 
   movieList: Object;
-  constructor (private data: DataService) { }
+  constructor (
+    private data: DataService,
+    private breakpointObserver: BreakpointObserver, 
+  ) { }
   ngOnInit( ) {
     this.data.getMovies().subscribe(data => {
+      this.loading = true;
       this.movieList = data;
-      console.log(this.movieList);
-    });
-  }
+      //Improve this, make a promise to get proper async loading state
+      if (this.movieList){
+        this.loading = false;
+      }
 
+      console.log(this.movieList);
+    })
+  }
+  
 }
 
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService, 
+  ) { }
 
   ngOnInit() {
+    console.log("getting users...");
+    this.authService.getCollection();
   }
 
 }
